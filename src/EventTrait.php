8a7a30f72cc4b1e41db25b47bbd15867279b1575
Barney Hanlon <shrikeh@gmail.com>
2017-07-13 14:33:56 +0100

    Create trait to aid in creating events easily

---
+++ src/EventTrait.php
@@ -0,0 +1,49 @@
+<?php
+namespace EventIO\InterOp;
+
+/**
+ * Trait EventTrait
+ * @package EventIO\InterOp
+ */
+trait EventTrait
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var boolean
+     */
+    private $propogationStopped = false;
+    
+
+    /**
+     * The name of the event
+     * @return string
+     */
+    public function name()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Check whether propagation was stopped.
+     *
+     * @return bool
+     */
+    public function isPropagationStopped(): bool
+    {
+        return ($this->propogationStopped);
+    }
+
+    /**
+     * @return self
+     */
+    public function stopPropagation(): self
+    {
+        $this->propogationStopped = true;
+
+        return $this;
+    }
+}
